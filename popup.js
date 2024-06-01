da35b3b0f8433200927fe05dc6c4af244e0bed34
Rahul Kumar <152762024+Astroinnovator@users.noreply.github.com>
2024-06-01 16:00:16 +0530

    ui improve

--- popup.js
+++ popup.js
@@ -1,9 +1,25 @@
 document.getElementById('shortenBtn').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const url = tabs[0].url;
-      chrome.runtime.sendMessage({ action: "shortenURL", url: url }, (response) => {
-        document.getElementById('status').textContent = `Short URL: ${response.shortUrl}`;
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const url = tabs[0].url;
+    chrome.runtime.sendMessage({ action: "shortenURL", url: url }, (response) => {
+      const shortUrl = response.shortUrl;
+      const statusDiv = document.getElementById('status');
+      statusDiv.textContent = `Short URL: ${shortUrl}`;
+      statusDiv.style.display = 'block';
+      copyToClipboard(shortUrl);
+      statusDiv.addEventListener('click', () => {
+        copyToClipboard(shortUrl);
       });
     });
   });
-  
+});
+
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+  alert('Short URL copied');
+}
