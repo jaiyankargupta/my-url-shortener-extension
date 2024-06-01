ca63ae32b004f3c77b5ebfcb4c9e72d134cb3f8c
Rahul Kumar <152762024+Astroinnovator@users.noreply.github.com>
2024-06-01 15:49:27 +0530

    improve the background.js code

--- background.js
+++ background.js
@@ -1,39 +1,34 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("Extension installed");
-    chrome.storage.local.set({ urlMapping: {}, counter: 1 });
-  });
-  
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "shortenURL") {
-      chrome.storage.local.get(["urlMapping", "counter"], (result) => {
-        const urlMapping = result.urlMapping || {};
-        let counter = result.counter || 1;
-  
-        const shortUrlKey = generateShortUrlKey(counter);
-        const shortUrl = chrome.runtime.getURL(`redirect.html?key=${shortUrlKey}`);
-  
-        urlMapping[shortUrlKey] = request.url;
-        counter += 1;
-  
-        chrome.storage.local.set({ urlMapping, counter }, () => {
-          sendResponse({ shortUrl });
-        });
+  console.log("Extension installed");
+  chrome.storage.local.set({ urlMapping: {}, counter: 1 });
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "shortenURL") {
+    chrome.storage.local.get(["urlMapping", "counter"], ({ urlMapping = {}, counter = 1 }) => {
+      const shortUrlKey = generateShortUrlKey(counter);
+      const shortUrl = `${chrome.runtime.getURL('')}?key=${shortUrlKey}`;
+      
+      urlMapping[shortUrlKey] = request.url;
+      
+      chrome.storage.local.set({ urlMapping, counter: counter + 1 }, () => {
+        sendResponse({ shortUrl });
       });
-      return true;  // Return true to indicate async response
-    }
-  });
-  
-  function generateShortUrlKey(counter) {
-    return toBase62(counter);
+    });
+    return true;  // Indicate async response
   }
-  
-  function toBase62(num) {
-    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let result = '';
-    while (num > 0) {
-      result = chars[num % 62] + result;
-      num = Math.floor(num / 62);
-    }
-    return result || '0';
-  }
-  
+});
+
+function generateShortUrlKey(counter) {
+  return toBase62(counter);
+}
+
+function toBase62(num) {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  do {
+    result = chars[num % 62] + result;
+    num = Math.floor(num / 62);
+  } while (num > 0);
+  return result;
+}
